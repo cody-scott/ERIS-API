edbded03af51855592ad36dbd6607b98b3897902
Cody <jcodyscott@gmail.com>
2022-03-23 15:33:28 -0400

    update to poetry

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 import setuptools
 
-setuptools.setup()
+import configparser
+config = configparser.ConfigParser()
+config.read('pyproject.toml')
+n = config['tool.poetry']['name'].replace('"','')
+v = config['tool.poetry']['version'].replace('"','')
+
+
+setuptools.setup(
+    name=n,
+    version=v
+)
